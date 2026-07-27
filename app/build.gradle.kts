91a2e363abc1d11584ec4c49330e0cdd786a84b6
junjiang.j <junjiang.j@bytedance.com>
2026-07-27 11:23:44 -0700

    chore: badge launcher icon with SDK version 4, bump to 1.0.6 (vc 6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.baijiahu.test.age.signal"
         minSdk = 23
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.5"
+        versionCode = 6
+        versionName = "1.0.6"
 
         buildConfigField("String", "AGE_SIGNALS_VERSION", "\"$ageSignalsVersion\"")
     }
